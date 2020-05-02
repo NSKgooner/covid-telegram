44a8e1e2a0c929287db55a0da15833ce246c9496
alexkryu4kov <alexkryu4kov@gmail.com>
2020-05-02 17:28:53 +0700

    Добавлена кнопка

--- echo-bot.py
+++ echo-bot.py
@@ -12,12 +12,24 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply('Hi!')
+    await message.reply('Привет! Введи название страны или региона РФ')
+
+
+@dp.message_handler(commands=['switch'])
+def switch(message: types.Message):
+    markup = types.InlineKeyboardMarkup()
+    switch_button = types.InlineKeyboardButton(text='Try', switch_inline_query='Telegram')
+    markup.add(switch_button)
+    await message.answer(message.chat.id, 'Выбрать чат', reply_markup=markup)
 
 
 @dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(predicts.get(message.text))
+async def handler(message: types.Message):
+    try:
+        await message.answer(predicts[message.text])
+    except KeyError:
+        await message.answer(message.text)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
