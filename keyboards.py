95faa1ab6b7a8bb1d843690f7492b286039e34b1
alexkryu4kov <alexkryu4kov@gmail.com>
2020-05-03 17:17:28 +0700

    Еще одна кнопка

--- keyboards.py
+++ keyboards.py
@@ -7,6 +7,8 @@ from aiogram.types import (
 )
 
 button_hi = KeyboardButton('Привет!')
+button_help = KeyboardButton('Помочь?')
 
-greet_kb = ReplyKeyboardMarkup()
+greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 greet_kb.add(button_hi)
+greet_kb.add(button_help)
