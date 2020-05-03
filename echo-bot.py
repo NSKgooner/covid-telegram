ac9be468d805f2549255733ffbefb3932d873767
alexkryu4kov <alexkryu4kov@gmail.com>
2020-05-03 15:03:42 +0700

    Кнопка с приветствием

--- echo-bot.py
+++ echo-bot.py
@@ -1,6 +1,11 @@
 import os
 
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.types import ContentType, Message, ParseMode
+from aiogram.utils.markdown import bold, text
+
+from keyboards import greet_kb
 
 API_TOKEN = os.environ['API_TOKEN']
 
@@ -8,28 +13,26 @@ predicts = {'Новосибирская область': '800'}
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
 
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    await message.reply('Привет! Введи название страны или региона РФ')
-
-
-@dp.message_handler(commands=['switch'])
-async def switch(message: types.Message):
-    markup = types.InlineKeyboardMarkup()
-    switch_button = types.InlineKeyboardButton(text='Try', switch_inline_query='Telegram')
-    markup.add(switch_button)
-    await message.answer(message.chat.id, 'Выбрать чат', reply_markup=markup)
+async def send_welcome(message: Message):
+    await message.reply('Привет! Введи название страны или региона РФ', reply_markup=greet_kb)
 
 
 @dp.message_handler()
-async def handler(message: types.Message):
+async def handler(message: Message):
     try:
         await message.answer(predicts[message.text])
     except KeyError:
-        await message.answer(message.text)
+        await message.answer('Привет!\nИспользуй /help, чтобы узнать список доступных команд!')
 
+
+@dp.message_handler(content_types=ContentType.ANY)
+async def unknown_message(message: Message):
+    message_text = text(bold('Я могу ответить на команды /start, /help, sep="\n"'))
+    await message.answer(message_text, parse_mode=ParseMode.MARKDOWN)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
